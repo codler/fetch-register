6239e89cdb6a607c7cb20a0c137a63cd3e7f6f30
Han Lin Yap <codler@users.noreply.github.com>
2018-10-09 14:47:03 +0200

    add user-agent

--- README.md
+++ README.md
@@ -4,6 +4,12 @@ _Fetch just works_
 
 Add [`fetch`](https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API) globally on any js environment.
 
+## Install
+
+```
+npm install --save fetch-register
+```
+
 ## Usage
 
 Add once in beginning of your project
--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const realFetch = require("node-fetch");
-module.exports = function(url, options) {
+module.exports = function(url, options = {}) {
   if (/^\/\//.test(url)) {
     url = "https:" + url;
   }
+  options.headers = {
+    "user-agent":
+      "fetch-register/1.0 (+https://github.com/codler/fetch-register)",
+    ...options.headers
+  };
   return realFetch.call(this, url, options);
 };
 
